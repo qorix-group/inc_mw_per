0dd2e5b7ce1b8c9955ff5510815875712259b3de
Sven Bachmann <sven.bachmann.ext@qorix.ai>
2025-04-04 10:18:59 +0200

    kvs: use KvsBuilder instead of Kvs::open, see details
    Builder updates:
      * use JSON string as defaults instead of a file
      * allow to override filenames
      * allow to override the working directory
    
    Signed-off-by: Sven Bachmann <sven.bachmann.ext@qorix.ai>

--- tests/kvs_checksum_missing.rs
+++ tests/kvs_checksum_missing.rs
@@ -11,7 +11,7 @@
 
 //! # Verify KVS Open with missing Checksum
 
-use rust_kvs::{ErrorCode, InstanceId, Kvs, KvsValue, OpenNeedDefaults, OpenNeedKvs, SnapshotId};
+use rust_kvs::{ErrorCode, InstanceId, KvsBuilder, KvsValue, SnapshotId};
 
 mod common;
 use crate::common::TempDir;
@@ -22,11 +22,10 @@ fn kvs_checksum_missing() -> Result<(), ErrorCode> {
     let dir = TempDir::create()?;
     dir.set_current_dir()?;
 
-    let kvs = Kvs::open(
-        InstanceId::new(0),
-        OpenNeedDefaults::Optional,
-        OpenNeedKvs::Optional,
-    )?;
+    let kvs = KvsBuilder::create(InstanceId::new(0))?
+        .need_defaults(false)
+        .need_kvs(false)
+        .build()?;
 
     kvs.set_value("number", 123.0)?;
     kvs.set_value("bool", true)?;
@@ -51,11 +50,11 @@ fn kvs_checksum_missing() -> Result<(), ErrorCode> {
     std::fs::remove_file(hash_filename)?;
 
     // opening must fail because of the missing checksum file
-    let kvs = Kvs::open(
-        InstanceId::new(0),
-        OpenNeedDefaults::Optional,
-        OpenNeedKvs::Required,
-    );
+    let kvs = KvsBuilder::create(InstanceId::new(0))?
+        .need_defaults(false)
+        .need_kvs(true)
+        .build();
+
     assert_eq!(kvs.err(), Some(ErrorCode::KvsHashFileReadError));
 
     Ok(())
